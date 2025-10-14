ae4caca966b4b86583ae000aca9097b791677265
okina3 <takosos@icloud.com>
2025-10-14 15:35:38 +0900

    マイグレーション、モデルの修正

--- src/database/migrations/2025_10_13_162405_create_admins_table.php
+++ src/database/migrations/2025_10_13_162405_create_admins_table.php
@@ -11,9 +11,32 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // 管理者情報テーブル
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
+        });
+
+        // 管理者用パスワードリセットテーブル
+        Schema::create('admin_password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        // 管理者用セッションテーブル
+        Schema::create('admin_sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('admin_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
@@ -23,5 +46,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('admins');
+        Schema::dropIfExists('admin_password_reset_tokens');
+        Schema::dropIfExists('admin_sessions');
     }
 };
