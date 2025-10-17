048dd2d9626d78a6cd73e78f8866cf28360ecd60
okina3 <takosos@icloud.com>
2025-10-17 19:08:34 +0900

    ダミーデータの作成

--- src/database/migrations/2025_10_17_165146_create_images_table.php
+++ src/database/migrations/2025_10_17_165146_create_images_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->foreignId('user_id')
                 ->constrained()
                 ->onUpdate('cascade')
