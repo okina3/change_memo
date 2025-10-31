4a4c799da122645cd1420c368b187413344d82cf
okina3 <takosos@icloud.com>
2025-10-31 12:56:09 +0900

    ダミーデータを文字列から数字に変更し、シーダーファイルを更新しました。

--- src/database/seeders/ImageSeeder.php
+++ src/database/seeders/ImageSeeder.php
@@ -16,51 +16,51 @@ class ImageSeeder extends Seeder
         DB::table('images')->insert([
             [
                 //ユーザー１のダミーデータ
-                'user_id' => '1',
+                'user_id' => 1,
                 'filename' => 'sample_1.jpg',
             ],
             [
-                'user_id' => '1',
+                'user_id' => 1,
                 'filename' => 'sample_2.jpg',
             ],
             [
-                'user_id' => '1',
+                'user_id' => 1,
                 'filename' => 'sample_3.jpg',
             ],
             [
-                'user_id' => '1',
+                'user_id' => 1,
                 'filename' => 'sample_4.jpg',
             ],
             [
-                'user_id' => '1',
+                'user_id' => 1,
                 'filename' => 'sample_5.jpg',
             ],
 
             //ユーザー２のダミーデータ
             [
-                'user_id' => '2',
+                'user_id' => 2,
                 'filename' => 'sample_6.jpg',
             ],
             [
-                'user_id' => '2',
+                'user_id' => 2,
                 'filename' => 'sample_7.jpg',
             ],
             [
-                'user_id' => '2',
+                'user_id' => 2,
                 'filename' => 'sample_8.jpg',
             ],
 
             //ユーザー３のダミーデータ
             [
-                'user_id' => '3',
+                'user_id' => 3,
                 'filename' => 'sample_9.jpg',
             ],
             [
-                'user_id' => '3',
+                'user_id' => 3,
                 'filename' => 'sample_10.jpg',
             ],
             [
-                'user_id' => '3',
+                'user_id' => 3,
                 'filename' => 'sample_11.jpg',
             ],
         ]);
--- src/database/seeders/MemoBaitSeeder.php
+++ src/database/seeders/MemoBaitSeeder.php
@@ -15,72 +15,72 @@ class MemoBaitSeeder extends Seeder
         DB::table('memo_baits')->insert([
             //ユーザー１のダミーデータ
             [
-                'memo_id' => '1',
-                'bait_id' => '1',
+                'memo_id' => 1,
+                'bait_id' => 1,
             ],
             [
-                'memo_id' => '2',
-                'bait_id' => '2',
+                'memo_id' => 2,
+                'bait_id' => 2,
             ],
             [
-                'memo_id' => '3',
-                'bait_id' => '3',
+                'memo_id' => 3,
+                'bait_id' => 3,
             ],
 
             //ユーザー２のダミーデータ
             [
-                'memo_id' => '11',
-                'bait_id' => '4',
+                'memo_id' => 11,
+                'bait_id' => 4,
             ],
             [
-                'memo_id' => '12',
-                'bait_id' => '5',
+                'memo_id' => 12,
+                'bait_id' => 5,
             ],
             [
-                'memo_id' => '13',
-                'bait_id' => '6',
+                'memo_id' => 13,
+                'bait_id' => 6,
             ],
 
             //ユーザー３のダミーデータ
             [
-                'memo_id' => '16',
-                'bait_id' => '7',
+                'memo_id' => 16,
+                'bait_id' => 7,
             ],
             [
-                'memo_id' => '17',
-                'bait_id' => '8',
+                'memo_id' => 17,
+                'bait_id' => 8,
             ],
             [
-                'memo_id' => '18',
-                'bait_id' => '9',
+                'memo_id' => 18,
+                'bait_id' => 9,
             ],
 
             //ユーザー４のダミーデータ
             [
-                'memo_id' => '21',
-                'bait_id' => '10',
+                'memo_id' => 21,
+                'bait_id' => 10,
             ],
             [
-                'memo_id' => '22',
-                'bait_id' => '11',
+                'memo_id' => 22,
+                'bait_id' => 11,
             ],
             [
-                'memo_id' => '23',
-                'bait_id' => '12',
+                'memo_id' => 23,
+                'bait_id' => 12,
             ],
 
             //ユーザー５のダミーデータ
             [
-                'memo_id' => '26',
-                'bait_id' => '13',
+                'memo_id' => 26,
+                'bait_id' => 13,
             ],
             [
-                'memo_id' => '27',
-                'bait_id' => '14',
+                'memo_id' => 27,
+                'bait_id' => 14,
             ],
             [
-                'memo_id' => '28',
-                'bait_id' => '15',
+                'memo_id' => 28,
+                'bait_id' => 15,
             ],
         ]);
     }
