adcc5d9aa696d45faf84ae06c03a3bd535f887ac
okina3 <takosos@icloud.com>
2025-11-01 19:39:03 +0900

    モーダルウィンドウのインストールと初期設定

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -2,3 +2,10 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// es6 module
+import MicroModal from 'micromodal';
+MicroModal.init({
+   disableScroll: true
+}
+);
