1cf9122d3b9e717aa89610f86beeb379aa8495bc
okina3 <takosos@icloud.com>
2025-11-01 23:21:01 +0900

    コントローラーの修正

--- src/app/Http/Controllers/User/MemoController.php
+++ src/app/Http/Controllers/User/MemoController.php
@@ -109,9 +109,7 @@ class MemoController extends Controller
                 ]);
 
                 // 新規スポットの入力があれば、データを保存。
-                // SpotService::storeNewSpot($request->input('new_spot'), $memo);
-                // 既存のスポットの選択があれば、メモに紐付けて保存
-                MemoService::attachExistingSpot($request, $memo->id);
+                SpotService::storeNewSpot($request->input('new_spot'), $memo);
 
                 // 新規エサの入力があれば、各データを保存。
                 BaitService::storeNewBait($request->new_bait, $memo->id);
