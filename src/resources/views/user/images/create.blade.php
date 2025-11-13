7d7dab8cea715dbfc4da99ada05c2fca40b77a6d
okina3 <takosos@icloud.com>
2025-11-13 17:01:55 +0900

    戻るボタン画像のコンポーネント化

--- src/resources/views/user/images/create.blade.php
+++ src/resources/views/user/images/create.blade.php
@@ -25,12 +25,7 @@
                </div>
             </form>
             {{-- 戻るボタン --}}
-            <div class="mb-2 flex justify-end">
-               <button class="btn mr-1 bg-gray-800 hover:bg-gray-700"
-                  onclick="location.href='{{ route('user.image.index') }}'">
-                  戻る
-               </button>
-            </div>
+            <x-user.button.back-button-image />
          </div>
       </section>
    </div>
--- src/resources/views/user/shareSettings/show.blade.php
+++ src/resources/views/user/shareSettings/show.blade.php
@@ -29,12 +29,7 @@
             {{-- 選択した共有メモに紐づいた画像の表示 --}}
             <x-user.images.big-select-image :getMemoImages='$get_memo_images' />
             {{-- 戻るボタン --}}
-            <div class="mb-2 flex justify-end">
-               <button class="btn bg-gray-800 hover:bg-gray-700"
-                  onclick="location.href='{{ route('user.share-setting.index') }}'">
-                  戻る
-               </button>
-            </div>
+            <x-user.button.back-button-shared />
          </div>
       </section>
    </div>
