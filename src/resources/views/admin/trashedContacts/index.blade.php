5a98e24a5afec596d8c5a8c522efd5ce71fcdce4
okina3 <takosos@icloud.com>
2025-11-14 18:19:13 +0900

    管理者の共有の修正。

--- src/resources/views/admin/trashedContacts/index.blade.php
+++ src/resources/views/admin/trashedContacts/index.blade.php
@@ -3,25 +3,11 @@
       {{-- フラッシュメッセージ --}}
       <x-common.flash-message status="session('status')" />
       {{-- 検索の表示エリア --}}
-      <section class="mb-5 px-3 py-2 text-slate-100 border border-gray-500 rounded-lg bg-rose-900">
-         <form action="{{ route('admin.contact.index') }}" method="get">
-            <div class="sm:flex items-center">
-               <div class="heading">キーワードから検索</div>
-               <div class="hidden sm:block">・・・・・</div>
-               {{-- キーワードを入力 --}}
-               <input class="py-2 w-60 text-slate-900 border border-gray-500 rounded-lg" name="keyword"
-                  placeholder="キーワードを入力">
-               {{-- 検索するボタン --}}
-               <button class="ml-2 btn btn-bk bg-yellow-500 hover:bg-yellow-400">検索する</button>
-            </div>
-         </form>
-         {{-- コメント --}}
-         <p class="text-sm mt-2">※ キーワードは、件名、問い合わせ内容の、両方から検索します。</p>
-      </section>
+      <x-admin.search.contacts-search />
       {{-- ユーザーからの問い合わせ一覧の表示エリア --}}
       <section class="text-gray-600 border border-gray-500 rounded-lg overflow-hidden bg-white">
          {{-- タイトル --}}
-         <h1 class="heading heading_bg bg-rose-900">削除済みユーザーからの問い合わせ一覧</h1>
+         <h1 class="heading heading_bg bg-rose-900">削除済み問い合わせ一覧</h1>
          {{-- ユーザーからの問い合わせ一覧 --}}
          <div class="p-2 h-[73vh] overflow-y-scroll overscroll-none">
             @foreach ($all_trashed_contacts as $contact)
